68fd2da603e9971badbdc5b5e8c8cd1c6f433c0c
ApxNord <nord-life17@yandex.ru>
2022-07-29 20:43:20 +0300

    Добавьте файлы проекта.

---
+++ DependecyInjectorExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using DependecyInjectorExample;
+
+/*EmployeeBL employeeBl = new EmployeeBL(new EmployeeDAL());
+List<Employee> listEmployees = employeeBl.GetAllEmployees();
+*/
+/*
+EmployeeBL employeeBL = new EmployeeBL();
+employeeBL.employeeDataObject = new EmployeeDAL();
+*/
+EmplyeeBL emplyeeBL = new EmplyeeBL();
+
+List<Employee> listEmployees = emplyeeBL.GetAllEmployees(new EmployeeDAL());
+foreach (Employee emp in listEmployees)
+{
+    Console.WriteLine("ID = {0}, Name = {1}, Department = {2}",
+        emp.ID, emp.Name, emp.Department);
+}
+
+/*
+Unity
+Castle Windsor
+StructureMap
+Spring.NET
+ */
